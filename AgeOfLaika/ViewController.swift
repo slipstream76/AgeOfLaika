5dee7e495924bf87d87b29399ec1508cb7536af7
Christopher L Weaver <slipstream76@MiMac-3.local>
2015-06-23 19:52:59 -0500

    Initial Project Upload
    Signed-off-by: Christopher L Weaver <slipstream76@MiMac-3.local>

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var ageInHumanYearsTextField: UITextField!
+    @IBOutlet weak var ageInDogYearsLabel: UILabel!
+    @IBOutlet weak var inputErrorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+        // If no number is entered, display a warning
+        if ageInHumanYearsTextField.text == "" {
+            inputErrorLabel.hidden = false
+        }
+        else {
+            // Hide the warning
+            inputErrorLabel.hidden = true
+           
+            // Calculate dog years
+            let conversionConstant = 7
+            let dogYears = ageInHumanYearsTextField.text.toInt()! * conversionConstant
+            ageInDogYearsLabel.text = "\(ageInHumanYearsTextField.text)" + " human years is equivalent to " + "\(dogYears)" + " Dog Years."
+            ageInDogYearsLabel.hidden = false
+            ageInHumanYearsTextField.resignFirstResponder()
+        }
+    }
 }
+
 
